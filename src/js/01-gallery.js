8e5f09223900c59dfa56afce66a74596c924b536
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-02-16 21:40:55 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,42 +1,21 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
 
-// import { galleryItems } from './gallery-items.js';
-// // Change code below this line
 
-// const galleryEl = document.querySelector('.gallery');
+const galleryEl = document.querySelector('.gallery');
 
-// const imagesList = galleryItems.map(({ preview, original, description }) => {
+const imagesList = galleryItems.map(({ preview, original, description }) => {
     
-// return `<div class="gallery__item">
-//   <a class="gallery__link" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//     />
-//   </a>
-//     </div>`;
-// }).join('');
+return `<a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>`;
+}).join('');
 
-// galleryEl.insertAdjacentHTML('beforeend', imagesList);
-// galleryEl.addEventListener('click', onImgOriginalCard);
+galleryEl.insertAdjacentHTML('beforeend', imagesList);
 
-// function onImgOriginalCard(e) {
-//     e.preventDefault();
-//     if (!e.target.dataset.source) {
-//         return
-//     }
-    
-//     // console.log(e.target);
-    
-//     const instance = basicLightbox.create(`
-//     <img src="${e.target.dataset.source}" width="800" height="600">
-//     `)
-    
-//     instance.show();
-// };
+const lightbox = new SimpleLightbox('.gallery__item', { captionsData: "alt", captionDelay: 250, });
